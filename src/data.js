d04403412fe772cf7beaeba595c2f868285c63d6
Theo Abossolo <theo.abossolo@gmail.com>
2022-06-05 04:32:39 -0500

    slider done

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://static.zara.net/photos///2022/I/0/1/p/2143/070/800/2/w/312/2143070800_1_1_1.jpg?ts=1653686437261",
+    title: "SUMMER SALES!!",
+    desc: "DON'T COMPROMISE ON STYLES GET FLAT 30% FOR NEW ARRIVALS",
+  },
+  {
+    id: 2,
+    img: "https://us.benetton.com/on/demandware.static/-/Library-Sites-BenettonSharedLibrary/default/dw7ef64cf2/01-REDESIGN/GENDER/shopthelook-product-set-women.jpg",
+    title: "SUMMER SALES!!",
+    desc: "DON'T COMPROMISE ON STYLES GET FLAT 30% FOR NEW ARRIVALS",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51r-e9KH1+L._AC_SR400,600_.jpg",
+    title: "DON'T MISS THIS DEAL!!",
+    desc: "TAKE CARE OF YOUR LAWN WITH JOY!!",
+  },
+];
