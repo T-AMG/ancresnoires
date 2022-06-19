108e330ba0adcdd90c95e826d2e7bc5463b47977
Theo Abossolo <theo.abossolo@gmail.com>
2022-06-19 03:58:49 -0500

    categories done in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Annoucement from "../components/Annoucement";
+import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
@@ -8,6 +10,8 @@ const Home = () => {
       <Annoucement />
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </div>
   );
 };
