af6438a2c3060fb7bae612d840786ea83631bb3a
Theo Abossolo <theo.abossolo@gmail.com>
2022-06-19 04:54:31 -0500

    Newsletter component added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,20 @@ import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -13,6 +27,10 @@ const Container = styled.div`
   justify-content: center;
   background: #f5fbfd;
   position: relative;
+
+  &: hover ${Info} {
+    opacity: 1;
+  } ;
 `;
 const Circle = styled.div`
   width: 200px;
@@ -25,18 +43,7 @@ const Image = styled.img`
   height: 75%;
   z-index: 2;
 `;
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 const Icon = styled.div`
   width: 40px;
   height: 40px;
@@ -47,6 +54,7 @@ const Icon = styled.div`
   justify-content: center;
   margin: 10px;
   transition: all 0.5s ease;
+  cursor: pointer;
 
   &:hover {
     background-color: #e9f5f5;
