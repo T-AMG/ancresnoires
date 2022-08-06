240cc2bc3c0de34151dffcec17f887c7af5dd0be
Theo Abossolo <theo.abossolo@gmail.com>
2022-08-06 03:08:52 -0500

    register and login pages done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-//import Home from "./pages/Home";
-import Product from "./pages/Product";
-//import ProductsList from "./pages/Product";
+import Login from "./pages/Login";
 
 function App() {
-  return <Product />;
+  return <Login />;
 }
 
 export default App;
